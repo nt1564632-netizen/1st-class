f412174ea5b99dc3059daf0b83180b1411958219
nt1564632-netizen <nt1564632@gmail.com>
2026-06-05 09:10:53 +0530

    Question number 8

---
+++ Question8.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int r=6;
+    int perimeter=2*3.14*r;
+    int area=3.14*r*r;
+    printf("Perimeter of circle= %d\n",perimeter);
+    printf("Area of circle= %d",area);
+    return 0;
+}
