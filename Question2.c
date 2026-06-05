184a2be2d40c26a3e8a8945120a7f89161157d2b
nt1564632-netizen <nt1564632@gmail.com>
2026-06-05 09:42:59 +0530

    Question 2

--- Question2.c
+++ Question2.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 int main(){
     char name;
-    int dob,mob;
+    int dob;
+    long mob;
     printf("Enter your name: ");
-    scanf("%c",&name);
+    scanf("%s",&name);
     printf("Enter your DOB: ");
     scanf("%d",&dob);
     printf("Enter your mob: ");
-    scanf("%d",&mob);
-    printf("Name: %c",name);
-    printf("DOB: %d",dob);
-    printf("Mobile:%d ",mob);
+    scanf("%ld",&mob);
+    printf("Name: %c\n",name);
+    printf("DOB: %d\n",dob);
+    printf("Mobile:%ld\n ",mob);
     return 0;
 }
