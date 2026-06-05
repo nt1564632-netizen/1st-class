51277979d7681c2df432a47e0a93f6526774f681
nt1564632-netizen <nt1564632@gmail.com>
2026-06-05 09:07:46 +0530

    Question number 3

---
+++ Question3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int num1,num2,sum=0;
+    printf("Enter 1st number: ");
+    scanf("%d",&num1);
+    printf("Enter 2nd number: ");
+    scanf("%d",&num2);
+    sum = num1+num2;
+    printf("SUM= %d",sum);
+    return 0;
+}
