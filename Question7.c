a12af07e59a4b10f5681f99bbb975a932ba92690
nt1564632-netizen <nt1564632@gmail.com>
2026-06-05 09:10:04 +0530

    Question number 7

---
+++ Question7.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int height=7,width=6;
+    int perimeter=2*(height+width);
+    int area=height*width;
+    printf("Perimeter of rectangle= %d\n",perimeter);
+    printf("Area of rectangle= %d",area);
+    return 0;
+}
