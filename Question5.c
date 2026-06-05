47bcba3f345d67fd787919394e2825a004803e50
nt1564632-netizen <nt1564632@gmail.com>
2026-06-05 09:08:57 +0530

    Question number 5

---
+++ Question5.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    float a,b,divide;
+    printf("Enter two number: ");
+    scanf("%d %d",&a,&b);
+    divide=a/b;
+    printf("Divide= %.2f",divide);
+    return 0;
+}
